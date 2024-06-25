e784909340eb3b94fb14a3dc6dc74d280e9a496d
meimaodeyu <1193402785@qq.com>
2024-06-25 22:17:36 +0800

    Init

--- nndl/实践五要素和简单的线性回归模型/真实模型.py
+++ nndl/实践五要素和简单的线性回归模型/真实模型.py
@@ -29,37 +29,38 @@ def create_toy_data(func, interval, sample_num, noise=0.0, add_outlier=False, ou
 
     # 生成高斯分布的标签噪声
     # 使用paddle。normal生成0均值，noise标准差的数据
-    epsilon = paddle.normal(0.0, std=noise, shape=(y.shape[0]) )
+    epsilon = paddle.normal(0, noise, paddle.to_tensor(y.shape[0]))
     y = y + epsilon
 
-    if add_outlier:
-       outlier_num = int(len(y)*outlier_ratio)
-       if outlier_num != 0:
-         #使用paddle。randint生成服从均匀分布的，范围在【0，len（y）】的随机tensor
-          outlier_idx = paddle.randint(len(y),shape=[outlier_num])
-          y[outlier_idx] = y[outlier_idx] * 5
-    return X,y
+    if add_outlier:  # 生成额外的异常点
+        outlier_num = int(len(y) * outlier_ratio)
+        if outlier_num != 0:
+            outlier_idx = paddle.randint(len(y), shape=[outlier_num]) #使用paddle.randint生成服从均匀分布的范围在【0，len（y）】的随机tensor
+            y[outlier_idx] = y[outlier_idx] * 5
+
+    return X, y
 
 
 
 func = linear_func
-interval = (-10,10)
-train_num = 100 #训练样本数量
-test_num = 50 #测试样本数目
+interval = (-10, 10)
+train_num = 100  # 训练样本数目
+test_num = 50  # 测试样本数目
 noise = 2
-X_train,y_train = create_toy_data(func=func, interval=interval, sample_num=train_num, noise=noise, add_outlier=False,)
-X_test,y_test = create_toy_data(func=func, interval=interval, sample_num=test_num, noise=noise, add_outlier=False,)
+
+X_train, y_train = create_toy_data(func=func, interval=interval, sample_num=train_num, noise=noise, add_outlier=False)
+X_test, y_test = create_toy_data(func=func, interval=interval, sample_num=test_num, noise=noise, add_outlier=False)
 X_train_large, y_train_large = create_toy_data(func=func, interval=interval, sample_num=5000, noise=noise, add_outlier=False)
 
 #paddle.linspace返回一个tensor，tensor的值为在区间start和stop上均匀间隔的num值，输出tensor的长度为num
-X_underlying = paddle.linspace(interval[0], interval[1],train_num)
+X_underlying = paddle.linspace(interval[0], interval[1], train_num)
 y_underlying = linear_func(X_underlying)
 
-plt.scatter(X_train,y_train,marker='*',facecolors='none',edgecolors='#e4008f',s=50, label='train date')
-plt.scatter(X_test,y_test,marker='none',facecolors='#f19ec2', s=50, label='test date')
-plt.plot(X_underlying,y_underlying,c='#000000', label='underlying distribution')
-plt.legend(fontsize='x-large')#给图像加图例
-plt.savefig('ml-vis.pdf')
+plt.scatter(X_train, y_train, marker='*', facecolor="none", edgecolor='#e4007f', s=50, label="train data")
+plt.scatter(X_test, y_test, facecolor="none", edgecolor='#f19ec2', s=50, label="test data")
+plt.plot(X_underlying, y_underlying, c='#000000', label="underlying distribution")
+plt.legend(fontsize='x-large')  # 给图像加图例
+plt.savefig('ml-vis.pdf')  # 保存图像到PDF文件中
 plt.show()
 
 
