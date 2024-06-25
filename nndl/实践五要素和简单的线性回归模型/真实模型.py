04243237ce53bbb203c573ef333e4ba76d383ad9
meimaodeyu <1193402785@qq.com>
2024-06-25 22:11:16 +0800

    Init

--- nndl/实践五要素和简单的线性回归模型/真实模型.py
+++ nndl/实践五要素和简单的线性回归模型/真实模型.py
@@ -1,12 +1,12 @@
+import paddle
+from matplotlib import pyplot as plt
+
 #真实函数的参数缺省值 w=1.2，b=0.5
 def linear_func(x, w=1.2,b=0.5):
     y = w * x + b
     return y
 
-import paddle
-
-def creat_toy_data(func, interval, sample_num, noise=0.0, add_outlier=False, outlier_ratio = 0.001, ):
-
+def create_toy_data(func, interval, sample_num, noise=0.0, add_outlier=False, outlier_ratio=0.001):
 
     """根据给定的函数，生成样本
      输入：
@@ -17,8 +17,8 @@ def creat_toy_data(func, interval, sample_num, noise=0.0, add_outlier=False, out
         add——outlier： 是否生成异常值
         outlier——ratio： 异常值占比
      输出：
-        x：特征数据， shape=【n——samples，1】
-        y: 标签数据，shape=【n——samples，1】
+        x：特征数据， shape=【n_samples，1】
+        y: 标签数据，shape=【n_samples，1】
         :param sample_num:
      """
 
@@ -31,6 +31,7 @@ def creat_toy_data(func, interval, sample_num, noise=0.0, add_outlier=False, out
     # 使用paddle。normal生成0均值，noise标准差的数据
     epsilon = paddle.normal(0.0, std=noise, shape=(y.shape[0]) )
     y = y + epsilon
+
     if add_outlier:
        outlier_num = int(len(y)*outlier_ratio)
        if outlier_num != 0:
@@ -39,17 +40,17 @@ def creat_toy_data(func, interval, sample_num, noise=0.0, add_outlier=False, out
           y[outlier_idx] = y[outlier_idx] * 5
     return X,y
 
-from matplotlib import pyplot as plt
+
 
 func = linear_func
 interval = (-10,10)
 train_num = 100 #训练样本数量
 test_num = 50 #测试样本数目
 noise = 2
-X_train,y_train = creat_toy_data(func=func, interval=interval, sample_num=train_num, noise=noise, add_outlier=False,)
-X_test,y_test = creat_toy_data(func=func, interval=interval, sample_num=test_num, noise=noise, add_outlier=False,)
+X_train,y_train = create_toy_data(func=func, interval=interval, sample_num=train_num, noise=noise, add_outlier=False,)
+X_test,y_test = create_toy_data(func=func, interval=interval, sample_num=test_num, noise=noise, add_outlier=False,)
+X_train_large, y_train_large = create_toy_data(func=func, interval=interval, sample_num=5000, noise=noise, add_outlier=False)
 
-X_train_large, y_train_large = creat_toy_data(func=func, interval=interval, sample_num=5000, noise=noise, add_outlier=False)
 #paddle.linspace返回一个tensor，tensor的值为在区间start和stop上均匀间隔的num值，输出tensor的长度为num
 X_underlying = paddle.linspace(interval[0], interval[1],train_num)
 y_underlying = linear_func(X_underlying)
