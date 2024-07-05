49f57fe9459a94672e7507a91d360737404223ea
meimaodeyu <1193402785@qq.com>
2024-07-05 18:40:52 +0800

    Init

---
+++ nndl/多项式回归/基于线性回归的波士顿房价预测.py
@@ -0,0 +1,192 @@
+import pandas as pd # 开源数据分析和操作工具
+import matplotlib.pyplot as plt  # 可视化工具
+import paddle
+import paddle.nn as nn
+import os
+
+mse_loss = nn.MSELoss()
+paddle.seed(10)
+
+# 利用pandas加载波士顿房价的数据集
+data=pd.read_csv("nndl/多项式回归/基于线性回归的波士顿房价预测.py/boston_house_prices.csv")
+# 预览前5行数据
+data.head()
+# 查看各字段缺失值统计情况
+data.isna().sum()
+
+
+# 箱线图查看异常值分布
+def boxplot(data, fig_name):
+    # 绘制每个属性的箱线图
+    data_col = list(data.columns)
+
+    # 连续画几个图片
+    plt.figure(figsize=(5, 5), dpi=300)
+    # 子图调整
+    plt.subplots_adjust(wspace=0.6)
+    # 每个特征画一个箱线图
+    for i, col_name in enumerate(data_col):
+        plt.subplot(3, 5, i + 1)
+        # 画箱线图
+        plt.boxplot(data[col_name],
+                    showmeans=True,
+                    meanprops={"markersize": 1, "marker": "D", "markeredgecolor": '#f19ec2'},  # 均值的属性
+                    medianprops={"color": '#e4007f'},  # 中位数线的属性
+                    whiskerprops={"color": '#e4007f', "linewidth": 0.4, 'linestyle': "--"},
+                    flierprops={"markersize": 0.4},
+                    )
+        # 图名
+        plt.title(col_name, fontdict={"size": 5}, pad=2)
+        # y方向刻度
+        plt.yticks(fontsize=4, rotation=90)
+        plt.tick_params(pad=0.5)
+        # x方向刻度
+        plt.xticks([])
+    plt.savefig(fig_name)
+    plt.show()
+
+
+boxplot(data, 'ml-vis5.pdf')
+"""使用四分位值筛选出箱线图中分布的异常值，并将这些数据视为噪声，其将被临界值取代"""
+# 四分位处理异常值
+num_features = data.select_dtypes(exclude=['object', 'bool']).columns.tolist()
+
+for feature in num_features:
+    if feature == 'CHAS':
+        continue
+
+    Q1 = data[feature].quantile(q=0.25)  # 下四分位
+    Q3 = data[feature].quantile(q=0.75)  # 上四分位
+
+    IQR = Q3 - Q1
+    top = Q3 + 1.5 * IQR  # 最大估计值
+    bot = Q1 - 1.5 * IQR  # 最小估计值
+    values = data[feature].values
+    values[values > top] = top  # 临界值取代噪声
+    values[values < bot] = bot  # 临界值取代噪声
+    data[feature] = values.astype(data[feature].dtypes)
+
+# 再次查看箱线图，异常值已被临界值替换（数据量较多或本身异常值较少时，箱线图展示会不容易体现出来）
+boxplot(data, 'ml-vis6.pdf')
+
+
+
+# 划分训练集和测试集
+def train_test_split(X, y, train_percent=0.8):
+    n = len(X)
+    shuffled_indices = paddle.randperm(n)  # 返回一个数值在0到n-1、随机排列的1-D Tensor
+    train_set_size = int(n * train_percent)
+    train_indices = shuffled_indices[:train_set_size]
+    test_indices = shuffled_indices[train_set_size:]
+
+    X = X.values
+    y = y.values
+
+    X_train = X[train_indices]
+    y_train = y[train_indices]
+
+    X_test = X[test_indices]
+    y_test = y[test_indices]
+
+    return X_train, X_test, y_train, y_test
+
+
+X = data.drop(['MEDV'], axis=1)
+y = data['MEDV']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)  # X_train每一行是个样本，shape[N,D]
+
+"""为了消除纲量对数据特征之间影响，在模型训练前，需要对特征数据进行归一化处理，将数据缩放到[0, 1]区间内，使得不同特征之间具有可比性。"""
+X_train = paddle.to_tensor(X_train,dtype='float32')
+X_test = paddle.to_tensor(X_test,dtype='float32')
+y_train = paddle.to_tensor(y_train,dtype='float32')
+y_test = paddle.to_tensor(y_test,dtype='float32')
+
+X_min = paddle.min(X_train,axis=0)
+X_max = paddle.max(X_train,axis=0)
+
+X_train = (X_train-X_min)/(X_max-X_min)
+
+X_test  = (X_test-X_min)/(X_max-X_min)
+
+# 训练集构造
+train_dataset=(X_train,y_train)
+# 测试集构造
+test_dataset=(X_test)
+
+from nndl.Linear import Linear
+
+# 模型实例化
+input_size = 12
+model=Linear(input_size)
+
+from nndl.opitimizer import optimizer_lsm
+
+
+class Runner(object):
+    def __init__(self, model, optimizer, loss_fn, metric):
+        # 优化器和损失函数为None,不再关注
+
+        # 模型
+        self.model = model
+        # 评估指标
+        self.metric = metric
+        # 优化器
+        self.optimizer = optimizer
+
+    def train(self, dataset, reg_lambda, model_dir):
+        X, y = dataset
+        self.optimizer(self.model, X, y, reg_lambda)
+
+        # 保存模型
+        self.save_model(model_dir)
+
+    def evaluate(self, dataset, **kwargs):
+        X, y = dataset
+
+        y_pred = self.model(X)
+        result = self.metric(y_pred, y)
+
+        return result
+
+    def predict(self, X, **kwargs):
+        return self.model(X)
+
+    def save_model(self, model_dir):
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+
+        params_saved_path = os.path.join(model_dir, 'params.pdtensor')
+        paddle.save(model.params, params_saved_path)
+
+    def load_model(self, model_dir):
+        params_saved_path = os.path.join(model_dir, 'params.pdtensor')
+        self.model.params = paddle.load(params_saved_path)
+
+optimizer = optimizer_lsm
+
+# 实例化Runner
+runner = Runner(model, optimizer=optimizer,loss_fn=None, metric=mse_loss)
+
+# 模型保存文件夹
+saved_dir = 'D:\Workspace\pythonProject2\mdoels'
+
+# 启动训练
+runner.train(train_dataset,reg_lambda=0,model_dir=saved_dir)
+columns_list = data.columns.to_list()
+weights = runner.model.params['w'].tolist()
+b = runner.model.params['b'].item()
+
+for i in range(len(weights)):
+    print(columns_list[i],"weight:",weights[i])
+
+print("b:",b)
+# 加载模型权重
+runner.load_model(saved_dir)
+
+mse = runner.evaluate(test_dataset)
+print('MSE:', mse.item())
+runner.load_model(saved_dir)
+pred = runner.predict(X_test[:1])
+print("真实房价：",y_test[:1].item())
+print("预测的房价：",pred.item())
--- nndl/多项式回归/数据集构建.py
+++ nndl/多项式回归/数据集构建.py
@@ -5,8 +5,15 @@ import paddle
 def sin(x):
     y = paddle.sin(2*math.pi*x)
     return y
-def create_toy_data(func, interval, sample_num, noise=0.0, add_outlier=False, outlier_ratio = 0.001, ):
+import paddle
+from matplotlib import pyplot as plt
 
+#真实函数的参数缺省值 w=1.2，b=0.5
+def linear_func(x, w=1.2,b=0.5):
+    y = w * x + b
+    return y
+
+def create_toy_data(func, interval, sample_num, noise=0.0, add_outlier=False, outlier_ratio=0.001):
 
     """根据给定的函数，生成样本
      输入：
@@ -17,8 +24,8 @@ def create_toy_data(func, interval, sample_num, noise=0.0, add_outlier=False, ou
         add——outlier： 是否生成异常值
         outlier——ratio： 异常值占比
      输出：
-        x：特征数据， shape=【n——samples，1】
-        y: 标签数据，shape=【n——samples，1】
+        x：特征数据， shape=【n_samples，1】
+        y: 标签数据，shape=【n_samples，1】
         :param sample_num:
      """
 
@@ -29,33 +36,36 @@ def create_toy_data(func, interval, sample_num, noise=0.0, add_outlier=False, ou
 
     # 生成高斯分布的标签噪声
     # 使用paddle。normal生成0均值，noise标准差的数据
-    epsilon = paddle.normal(0.0, std=noise, shape=(y.shape[0]) )
+    epsilon = paddle.normal(0, noise, paddle.to_tensor(y.shape[0]))
     y = y + epsilon
-    if add_outlier:
-       outlier_num = int(len(y)*outlier_ratio)
-       if outlier_num != 0:
-         #使用paddle。randint生成服从均匀分布的，范围在【0，len（y）】的随机tensor
-          outlier_idx = paddle.randint(len(y),shape=[outlier_num])
-          y[outlier_idx] = y[outlier_idx] * 5
-    return X,y
-from  matplotlib import pyplot as plt
-# 生成数据
-func = sin
-interval = (0,1)
-train_num = 15
-test_num = 10
-noise = 0.5 #0.1 
-X_train, y_train = create_toy_data(func=func, interval=interval, sample_num=train_num, noise = noise)
-X_test, y_test = create_toy_data(func=func, interval=interval, sample_num=test_num, noise = noise)
 
-X_underlying = paddle.linspace(interval[0],interval[1],num=100)
-y_underlying = sin(X_underlying)
+    if add_outlier:  # 生成额外的异常点
+        outlier_num = int(len(y) * outlier_ratio)
+        if outlier_num != 0:
+            outlier_idx = paddle.randint(len(y), shape=[outlier_num]) #使用paddle.randint生成服从均匀分布的范围在【0，len（y）】的随机tensor
+            y[outlier_idx] = y[outlier_idx] * 5
 
-# 绘制图像
-plt.rcParams['figure.figsize'] = (8.0, 6.0)
-plt.scatter(X_train, y_train, facecolor="none", edgecolor='#e4007f', s=50, label="train data")
-#plt.scatter(X_test, y_test, facecolor="none", edgecolor="r", s=50, label="test data")
-plt.plot(X_underlying, y_underlying, c='#000000', label=r"$\sin(2\pi x)$")
-plt.legend(fontsize='x-large')
-plt.savefig('ml-vis2.pdf')
+    return X, y
+
+
+
+func = linear_func
+interval = (-10, 10)
+train_num = 100  # 训练样本数目
+test_num = 50  # 测试样本数目
+noise = 2
+
+X_train, y_train = create_toy_data(func=func, interval=interval, sample_num=train_num, noise=noise, add_outlier=False)
+X_test, y_test = create_toy_data(func=func, interval=interval, sample_num=test_num, noise=noise, add_outlier=False)
+X_train_large, y_train_large = create_toy_data(func=func, interval=interval, sample_num=5000, noise=noise, add_outlier=False)
+
+#paddle.linspace返回一个tensor，tensor的值为在区间start和stop上均匀间隔的num值，输出tensor的长度为num
+X_underlying = paddle.linspace(interval[0], interval[1], train_num)
+y_underlying = linear_func(X_underlying)
+
+plt.scatter(X_train, y_train, marker='*', facecolor="none", edgecolor='#e4007f', s=50, label="train data")
+plt.scatter(X_test, y_test, facecolor="none", edgecolor='#f19ec2', s=50, label="test data")
+plt.plot(X_underlying, y_underlying, c='#000000', label="underlying distribution")
+plt.legend(fontsize='x-large')  # 给图像加图例
+plt.savefig('ml-vis.pdf')  # 保存图像到PDF文件中
 plt.show()
